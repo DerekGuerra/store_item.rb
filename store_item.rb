0a15e70206b716ac2b5f2ef3501a79b606d7262d
“DerekE” <d12guerra@gmail.comm>
2019-02-19 20:34:48 -0600

    added attr_readers, attr_writers, and used symbols for one argument rather than multiple

--- store_item.rb
+++ store_item.rb
@@ -8,135 +8,63 @@
 
 
 class Car
-	def initialize(make, model, color, price)
-		@make = make 
-		@model = model 
-		@color = color
-		@price = color
-	end
 
-	def make 
-		return @make 
-	end
+		attr_reader :make, :model, :color, :price 
+		attr_writer :make, :model, :color, :price 
 
-	def model
-		return @model
-	end
+		def initialize(input_options)
+				@make = input_options[:make] 
+				@model = input_options[:model] 
+				@color = input_options[:color]
+				@price = input_options[:price]
+		end
 
-	def color
-		return @color
-	end
+	# def make 
+	# 	return @make 
+	# end
 
-	def price 
-		return @price
-	end
+	# def model
+	# 	return @model
+	# end
 
-	def info
-		p "This brand new #{color} #{make} #{model} is priced at #{price}!"
-	end
+	# def color
+	# 	return @color
+	# end
+
+	# def price 
+	# 	return @price
+	# end
+
+		def info
+				return "This brand new #{color} #{make} #{model} is priced at #{price}!"
+		end
 
 	#setters
 
-	def make=(text)
-		@make = text 
-	end
+	# def make=(text)
+	# 	@make = text 
+	# end
 
-	def model=(text)
-		@model = text
-	end
+	# def model=(text)
+	# 	@model = text
+	# end
 
-	def color=(text)
-		@color = text
-	end
+	# def color=(text)
+	# 	@color = text
+	# end
 
-	def price=(text)
-		@price = text
-	end
+	# def price=(text)
+	# 	@price = text
+	# end
 end
 
-car = Car.new("Ford", "F-150", "red", 15000)
-
-car.make = "Buick"
-car.model = "Verano"
+car = Car.new(
+{
+	:make => "GMC", 
+ :model => "Trax", 
+ :color => "Blue", 
+ :price => 1536352
+}
+)
 
 p car.info
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# class Employee
-# 	def initialize(first_name, last_name, salary, active_status)
-# 		@first_name = first_name
-# 		@last_name = last_name
-# 		@salary = salary
-# 		@active_status = active_status
-# 	end
-
-# 	# getter method
-# 	def first_name
-# 		return @first_name
-# 	end
-
-# 	def last_name
-# 		return @last_name
-# 	end
-
-# 	def salary
-# 		return @salary
-# 	end
-
-# 	def info
-# 		p "#{first_name} #{last_name} makes $#{salary} per year"
-# 	end
-
-# 	def give_raise
-# 		@salary *= 1.05
-# 	end
-# 	# /getter methods
-
-# 	# setter methoids
-
-# 	def first_name=(text)
-# 		@first_name = text
-# 	end
-
-# end
-
-
-# employee1 = Employee.new("Danilo", "Campos", 70000, false)
-# employee1.info
-
-# employee1.give_raise
-# employee1.info
-
-# employee2 = Employee.new("Majora", "Carter", 80000, true)
-# employee2.info
-
-# employee2.give_raise
-# employee2.info
-
-# p employee1.first_name
-# p employee1.first_name = "Bob"
-
-# p employee1.first_name
